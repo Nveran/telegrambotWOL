1f16b69170853daa9210824c082c983fec0ad5a8
Nveran <gysarKOT@gmail.com>
2021-02-01 21:55:31 +0300

    Добавлена возможность записи логов

--- telegrambot_wol.py
+++ telegrambot_wol.py
@@ -3,29 +3,26 @@ import telebot
 from wakeonlan import send_magic_packet
 from time import sleep
 
-
 # работа с файлами settings и token
 config = configparser.ConfigParser()  # создаём объекта парсера
-config.read('settings.ini')     # читаем настройки
-config.read("token.ini")    # читаем токен
+config.read('settings.ini')  # читаем настройки
+config.read("token.ini")  # читаем токен
 
 # подключение токена
 bot = telebot.TeleBot(config['Token']['token'])
-
 
 # подключение переменных из файла settings.ini
 USER = config['telegrambotWOL']['user']
 MAC_address = config['telegrambotWOL']['mac_address']
 IP_address = config['telegrambotWOL']['ip_address']
 PORT = int(config['telegrambotWOL']['port'])
-
+logs = config.getboolean('telegrambotWOL', 'logs')
 
 # переменные для кнопок клавиатуры и реакции на них
 no_msg = 'нет'
 yes_msg = 'да'
 on_computer_msg = 'включи компьютер'
 work_check_msg = 'проверка работы бота'
-
 
 # включение кнопок клавиатуры в боте
 keyboard1 = telebot.types.ReplyKeyboardMarkup()
@@ -34,40 +31,52 @@ keyboard1.row(yes_msg, no_msg)
 keyboard2.row(on_computer_msg, work_check_msg)
 
 
+# если Logs True пишет данные из консоли в файл, если нет, то просто в консоль
+def logs_or_print(data):
+    if logs:
+        print(data)
+        logs_file = open('logs.txt', 'a')
+        logs_file.write(data + '\n')
+        logs_file.close()
+    else:
+        print(data)
+
+
 # поведение при команде /start
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    if USER == '0':     # проверка первого запуска и отправка инструкции
+    if USER == '0':  # проверка первого запуска и отправка инструкции
         bot.send_message(message.chat.id, 'Приветствую, это первый запуск telegrambotWOL \nВаш id: ' + str(
             message.chat.id) + '\nПожалуйста, укажите его в файле settings.ini, следуя примечаниям в файле, затем,'
                                ' перезапустите программу и отправьте команду\n /start')
-        print('Приветствую, это первый запуск telegrambotWOL \nВаш id: ' + str(
+        logs_or_print('Приветствую, это первый запуск telegrambotWOL \nВаш id: ' + str(
             message.chat.id) + '\nПожалуйста, укажите его в файле settings.ini, следуя примечаниям в файле, затем,'
                                ' перезапустите программу и отправьте команду\n /start')
     elif str(message.chat.id) == USER:  # проверка на соответсвие id user
         bot.send_message(message.chat.id, 'Приветствую, администратор! Хотите включить компьтер?',
                          reply_markup=keyboard1)  # отправка сообщения в telegram
-        print('обнаружен администратор: ' + str(message.chat.id))  # отправка id пользователя в консоль
+        logs_or_print('обнаружен администратор: ' + str(message.chat.id))  # отправка id пользователя в консоль
     elif str(message.chat.id) != USER:  # проверка id администратора
-        print('обнаружен неизвестный пользователь: ' + str(message.chat.id))    # отправка id пользователя в консоль
+        logs_or_print('обнаружен неизвестный пользователь: ' + str(message.chat.id))
+        # отправка id пользователя в консоль
 
 
 # поведение при ответах на команды
 @bot.message_handler(content_types=['text'])
 def send_text(message):
-    if str(message.chat.id) == USER:    # проверка id администратора
+    if str(message.chat.id) == USER:  # проверка id администратора
         if message.text.lower() in (on_computer_msg, yes_msg):
             bot.send_message(message.chat.id, 'сейчас будет ;)', reply_markup=keyboard2)
             send_magic_packet(MAC_address, ip_address=IP_address,
-                              port=PORT)   # отправка пакета на включение PC
-            print('пользователь ' + str(message.chat.id) + ' включил компьютер')
+                              port=PORT)  # отправка пакета на включение PC
+            logs_or_print('пользователь ' + str(message.chat.id) + ' включил компьютер')
         elif message.text.lower() == no_msg:
             bot.send_message(message.chat.id, 'ну ладно :(', reply_markup=keyboard2)
         elif message.text.lower() == work_check_msg:
             bot.send_message(message.chat.id, 'я тут!', reply_markup=keyboard2)
-            print('бот работает успешно!')
+            logs_or_print('бот работает успешно!')
     elif str(message.chat.id) != USER:  # проверка id администратора
-        print('обнаружен неизвестный пользователь: ' + str(message.chat.id))
+        logs_or_print('обнаружен неизвестный пользователь: ' + str(message.chat.id))
 
 
 # метод связи с API Telegram
